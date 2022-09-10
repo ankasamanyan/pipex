d9dfddbe1e8f34bbed91a54be79a4997f0de0cad
Anahit K <anikakasamanyan@gmail.com>
2022-09-10 17:55:01 +0200

    almost there :)

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akasaman <akasaman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 02:43:57 by ankasamanya       #+#    #+#             */
-/*   Updated: 2022/09/09 18:03:22 by akasaman         ###   ########.fr       */
+/*   Updated: 2022/09/10 17:51:52 by ankasamanya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*find_big_path(char	**env)
 	while (*env)
 		if (ft_strncmp(*env++, "PATH=", 5) == 0)
 			return (*(env - 1) + 5);
-	return (NULL); //???????
+	return (NULL);
 }
 
 char	*find_lil_path(char *big_path, t_vars *vars)
@@ -28,20 +28,24 @@ char	*find_lil_path(char *big_path, t_vars *vars)
 	smoll_pathsies = ft_split(big_path, ':');
 	while (*smoll_pathsies)
 	{
+		vars->command = ft_split(vars->argv[vars->count], ' ');
 		lil_path = ft_strjoin(*smoll_pathsies++, ft_strjoin("/", vars->command[0]));
-		if (access( lil_path, F_OK) == 0)
+		if (access( lil_path, X_OK) == 0)
 			vars->full_path = lil_path;
 	}
-	// while (*smoll_pathsies)
-	// 	free(*smoll_pathsies++);
+	while (*smoll_pathsies)
+		free(*smoll_pathsies++);
 	return (NULL); //???????
 }
 
-void	execute_command(t_vars *vars, char **env)
+
+void	kiddi_process(t_vars *vars)
 {
-	vars->command = ft_split(vars->argv[vars->count], ' ');
-	execve(vars->full_path, vars->command, env);
-	printf("halp");
+	dup2(vars->input_file, STDIN_FILENO);
+	// dup2(vars->pipe[WRITE_PIPE], STDOUT_FILENO); //output rediraction 
+	close(vars->pipe[READ_PIPE]);
+	//execute da thingy in da fork();
+	execve(vars->full_path, vars->command, vars->env);
 }
 
 int main(int argc, char *argv[], char *env[])
@@ -49,28 +53,35 @@ int main(int argc, char *argv[], char *env[])
 	t_vars	vars;
 	// char	*big_path;
 	vars.argv = argv;
-	vars.count = 1; //временно
+	vars.env = env;
+	vars.count = 2; //временно
 	if (argc == 0)
 		return 0;
 	// printf("%s\n", argv[0]);
 	
-	// big_path = find_big_path(env);
-	find_lil_path(find_big_path(env), &vars);
-	//execute da thingy
-	execute_command(&vars, env);
-	if (vars.full_path)
-		printf("\nthis would be the lil path: %s\n",vars.full_path);
-
-	// int i;
+	// printf("\nthis would be the lil path: %s\n",vars.full_path);
 	
-	// i = 1;
-	// while (i++ <= argc - 2 )
-	// {
-	// 	//найти комманду в энвп
-		
-	// 	//сплит
-		
-	// }
+	// if (vars.full_path)
+	// 	printf("\nthis would be the lil path: %s\n",vars.full_path);
+	vars.input_file = open(argv[1], O_RDONLY);
+	vars.output_file = open("file2.txt", O_WRONLY, O_TRUNC, O_CREAT, 777);
+	
+	int i;
+	
+	i = 1;
+	while (i++ <= argc - 2 )
+	{
+		pipe(vars.pipe); 
+		find_lil_path(find_big_path(env), &vars);
+		vars.pid = fork();
+		if (vars.pid == 0)
+			kiddi_process(&vars);
+		else
+		{
+			close(vars.pipe[WRITE_PIPE]);
+			wait(0);
+		}
+	}
 	
 	// while(*env)
 	// 	printf("%s\n",*env++);
--- pipex.h
+++ pipex.h
@@ -3,28 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akasaman <akasaman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 02:43:49 by ankasamanya       #+#    #+#             */
-/*   Updated: 2022/09/09 18:01:52 by akasaman         ###   ########.fr       */
+/*   Updated: 2022/09/10 16:11:15 by ankasamanya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include "Libft/libft.h"
+# include <fcntl.h>
+
+# define READ_PIPE 0
+# define WRITE_PIPE 1
 
 typedef struct s_vars
 {
     char    **command;
+	char	**env;
     char    *full_path;
     char    **argv;
     int     count;
+	int		pipe[2];
+	int		input_file;
+	int		output_file;
+	int		pid;
 }   t_vars;
+
 /* find paths */
 char	*find_big_path(char	**env);
 char	*find_lil_path(char *big_path, t_vars *vars);
 /* command */
-void	execute_command(t_vars *vars, char **env);
+void	kiddi_process(t_vars *vars);
+
 /* free stuff */
 #endif
